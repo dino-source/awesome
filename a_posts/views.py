fbf2140444dd624cca38a4996b16b13d0d01469c
Dino Source <dinosource.cpp@gmail.com>
2025-07-22 06:31:27 -0700

    Modify title to demonstrate how Django uses 'jinja-like' variables

--- a_posts/views.py
+++ a_posts/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'index.html')
+    title = 'Welcome to  Django!'
+    return render(request, 'index.html', {'title' : title})
