cd36985b463c0611a010082aaf7c83ec2d0b63c4
Dino Source <dinosource.cpp@gmail.com>
2025-07-23 04:30:11 -0700

    Add 'delete post' functionality

--- a_core/urls.py
+++ a_core/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from a_posts.views import home_view, post_create_view
+from a_posts.views import home_view, post_create_view, post_delete_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_view, name="home"),
     path("post/create/", post_create_view, name="post-create"),
+    path("post/delete/<pk>/", post_delete_view, name="post-delete"),
 ]
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from django import forms
 from django.forms.models import ModelForm
 from django.shortcuts import render, redirect
@@ -5,14 +7,14 @@ from .models import Post
 
 
 def home_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all()  # type: ignore
     return render(request, "a_posts/home.html", {"posts": posts})
 
 
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ["url", "body"]
         labels = {
             "body": "Caption",
         }
@@ -24,6 +26,11 @@ class PostCreateForm(ModelForm):
                     "class": "font1 text-4xl",
                 }
             ),
+            "url": forms.TextInput(
+                attrs={
+                    "placeholder": "Add url...",
+                }
+            ),
         }
 
 
@@ -32,7 +39,34 @@ def post_create_view(request):
     if request.method == "POST":
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+
+            website = requests.get(form.data["url"])
+            html_sourcecode = BeautifulSoup(website.text, "html.parser")
+
+            find_image = html_sourcecode.select("meta[content^='https://live.staticflickr.com/']")
+            image = find_image[0]["content"]
+            post.image = image
+
+            find_title = html_sourcecode.select("h1.photo-title")
+            title = find_title[0].text.strip()
+            post.title = title
+
+            find_artist = html_sourcecode.select("a.owner-name")
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+
+            post.save()
             return redirect("home")
 
     return render(request, "a_posts/post_create.html", {"form": form})
+
+
+def post_delete_view(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect("home")
+
+    return render(request, "a_posts/post_delete.html", {"post": post})
+
