9ae9808e796fff761d66069d1e15d1360baeace6
Dino Source <dinosource.cpp@gmail.com>
2025-07-22 23:54:14 -0700

    Add functionality to create posts from http://localhost:8000/admin/ page

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home_view(request):
-    return render(request, 'a_posts/home.html')
+    posts = Post.objects.all()
+    return render(request, "a_posts/home.html", {"posts": posts})
+
