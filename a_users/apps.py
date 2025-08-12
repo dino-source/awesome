598a9b2bccff5650d218387a0bc6757a3bdec8fb
Dino Source <dinosource.cpp@gmail.com>
2025-08-12 00:51:06 -0700

    Add signals

--- a_users/apps.py
+++ a_users/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class AUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'a_users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "a_users"
+
+    def ready(self) -> None:
+        import a_users.signals
