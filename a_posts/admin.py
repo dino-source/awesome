b990bdcb05e6b3848e934ac0e71e1743d300c8bc
Dino Source <dinosource.cpp@gmail.com>
2025-08-13 05:38:23 -0700

    Add new feature: let users to reply to comments

--- a_posts/admin.py
+++ a_posts/admin.py
@@ -3,9 +3,11 @@ from .models import (
     Post,
     Tag,
     Comment,
+    Reply,
 )
 
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comment)
+admin.site.register(Reply)
